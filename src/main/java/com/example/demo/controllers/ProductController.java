4a8abfbe4e9f1ba224d3bafd899fa9a32cd89d85
Vitaliy <vitalijpupcenko@gmail.com>
2024-06-19 00:53:02 +0300

    Add images

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -7,6 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class ProductController {
@@ -17,20 +21,25 @@ public class ProductController {
     }
 
     @GetMapping("/")
-    public String products(Model model){
-        model.addAttribute("products", productService.listProducts());
+    public String products(@RequestParam(name = "title", required = false) String title, Model model){
+        model.addAttribute("products", productService.listProducts(title));
         return "products";
     }
 
     @GetMapping("/product/{id}")
-    public String infoProduct(@PathVariable Long id, Model model){
-        model.addAttribute("product", productService.getProductBuID(id));
+    public String productInfo(@PathVariable Long id, Model model){
+        Product product = productService.getProductBuId(id);
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
         return "product_info";
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product){
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam("file1") MultipartFile file1,
+                                @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3,
+                                Product product) throws IOException {
+        productService.saveProduct(product, file1, file2, file3);
         return "redirect:/";
     }
 
@@ -39,5 +48,4 @@ public class ProductController {
         productService.deleteProduct(id);
         return "redirect:/";
     }
-    int i;
 }
